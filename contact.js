a4f9d0226929c3b4f40f5803b5efaa5b5bc8d646
AlleenVandaag <alleen-vandaag@outlook.com>
2025-09-04 07:32:59 +0200

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,3 +1,15 @@
+function toggleMenu() {
+    document.getElementById("nav-list").classList.toggle("show");
+}
+const boldWords = document.querySelectorAll("footer b");
+
+boldWords.forEach(word => {
+  word.addEventListener("mouseover", () => {
+    word.classList.add("bounce");
+    setTimeout(() => word.classList.remove("bounce"), 600); // reset na animatie
+  });
+});
+
 document.getElementById('contact-form').addEventListener('submit', function(event) {
   event.preventDefault();
 
