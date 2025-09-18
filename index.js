090597c53ec5efbef0a12a6b67b5c8de5a01350f
AlleenVandaag <alleen-vandaag@outlook.com>
2025-09-18 08:57:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const boldWords = document.querySelectorAll("footer b");
 boldWords.forEach(word => {
   word.addEventListener("mouseover", () => {
     word.classList.add("bounce");
-    setTimeout(() => word.classList.remove("bounce"), 600); // reset na animatie
+    setTimeout(() => word.classList.remove("bounce"), 600); 
   });
+});
+
+function toggleMenu() {
+    document.getElementById("nav-list").classList.toggle("show");
+}
+
+const dropdownParents = document.querySelectorAll("nav ul li.has-dropdown > a");
+
+dropdownParents.forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault(); 
+        const dropdown = link.nextElementSibling;
+        dropdown.classList.toggle("show");
+    });
 });
